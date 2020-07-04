2734b67ae9f3ef95db354aba224f45dc8f36aa5e
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-04 13:37:27 +0300

    Solve task about alert box.

---
+++ alert/main.py
@@ -0,0 +1,35 @@
+import math
+from selenium import webdriver
+from time import sleep
+
+
+def calc(x: str):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+PAGE_URL = "http://suninjuly.github.io/alert_accept.html"
+
+driver = webdriver.Firefox()
+driver.get(PAGE_URL)
+
+# call alert box
+submit_btn = driver.find_element_by_css_selector("[type=\"submit\"]")
+submit_btn.click()
+
+alert = driver.switch_to_alert()
+alert.accept()
+
+sleep(1)
+
+# get elements
+answer_input = driver.find_element_by_id("answer")
+submit_btn = driver.find_element_by_css_selector("[type=\"submit\"]")
+
+value = driver.find_element_by_id("input_value").text
+answer = calc(value)
+
+# type answer
+answer_input.send_keys(answer)
+submit_btn.click()
+
+# driver.close()
