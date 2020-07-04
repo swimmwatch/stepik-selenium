950ea7decdc0810de960727fcc3c550110ffec9a
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-07-04 11:34:21 +0300

    Solve task with checkbox and radiobutton

--- checkbox-radiobutton/main.py
+++ checkbox-radiobutton/main.py
@@ -11,6 +11,19 @@ PAGE_URL = "http://suninjuly.github.io/math.html"
 driver = webdriver.Firefox()
 driver.get(PAGE_URL)
 
+# get elements
+answer_input = driver.find_element_by_id("answer")
+robot_checkbox = driver.find_element_by_id("robotCheckbox")
+robot_radiobtn = driver.find_element_by_id("robotsRule")
+submit_btn = driver.find_element_by_css_selector("[type=\"submit\"]")
 
+value = driver.find_element_by_id("input_value").text
+answer = calc(value)
 
-driver.close()
+# type answer
+answer_input.send_keys(answer)
+robot_checkbox.click()
+robot_radiobtn.click()
+submit_btn.click()
+
+# driver.close()
